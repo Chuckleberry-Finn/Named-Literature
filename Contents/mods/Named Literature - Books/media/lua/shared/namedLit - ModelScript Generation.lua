22a09625f07d76ee714caa3c910caa617067a37f
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-08-10 14:54:00 -0400

    hide model generation

--- Contents/mods/Named Literature - Books/media/lua/shared/namedLit - ModelScript Generation.lua
+++ Contents/mods/Named Literature - Books/media/lua/shared/namedLit - ModelScript Generation.lua
@@ -1,11 +1,12 @@
+--[[
 local function generateBookModels()
     local iterations = 27
     for n=1, iterations do
         local script = "model namedLitBook"..n.."Ground {mesh = WorldItems/BookClosed,texture = modelTexture/namedLitBookTexture"..n..",scale = 0.4,}"
         --"namedLitBook"..n.."Ground"
-        --[[DEBUG]] print("DEBUG: namedLit: script generated: "..script)
+        print("DEBUG: namedLit: script generated: "..script)
         getScriptManager():ParseScript(script)
     end
 end
 Events.OnGameBoot.Add(generateBookModels)
-
+--]]
